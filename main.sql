ca05b2bba84558041faa1c59ca170416b26e4bbb
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-11 20:30:58 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,7 @@
+/* Answer to: "" */
+
+/*
+  MongoDB is a NoSQL database that is more advanced and capable of
+  handling more data. SQL Server is a database management system
+  that is used to manage the relational database system.
+*/
